bf9f1610b0f69c3c178f94b5f64fcd03b7758554
nicknt <nikolasderm@gmail.com>
2024-12-01 13:16:47 +0200

    day 1: Pretty easy problem. Ready for the next one!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,80 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // creating 2 lists that we will fill with the rows
+        val rightList: MutableList<Int> = emptyList<Int>().toMutableList()
+        val leftList: MutableList<Int> = emptyList<Int>().toMutableList()
+
+        for (line in input) {
+            if (line.isEmpty()) break
+            // split the values
+            val splited = line.split("   ").map { it.toInt() }
+            // add the values to the lists
+            rightList.add(splited[0])
+            leftList.add(splited[1])
+        }
+        // Sort the lists. This is required cause then I am able to
+        // check the items one by one. Hot stuff
+        rightList.sort()
+        leftList.sort()
+
+        var sum = 0
+        for (i in 0..rightList.lastIndex) {
+            // getting the abs diff of the elements
+            val dist = abs(leftList[i] - rightList[i])
+            // add to the sum
+            sum += dist
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        // create the same 2 lists to store the first values
+        val rightList: MutableList<Int> = emptyList<Int>().toMutableList()
+        val leftList: MutableList<Int> = emptyList<Int>().toMutableList()
+
+        // also add a map to save the num of shows of each element
+        val shows: MutableMap<Int, Int> = emptyMap<Int, Int>().toMutableMap()
+
+        for (line in input) {
+            if (line.isEmpty()) break
+            // split the values
+            val splited = line.split("   ").map { it.toInt() }
+            // add the values to the lists
+            rightList.add(splited[0])
+            leftList.add(splited[1])
+        }
+
+        var sum = 0
+        for (i in 0..rightList.lastIndex) {
+            // get the current element from the right list
+            val element: Int = rightList[i]
+            // if the item is not on the map, that means we need to calculate the shows
+            if (!shows.containsKey(element)) {
+                // init the map to 0
+                shows[element] = 0
+                // for loop on the list and increment the counter
+                leftList.forEach {
+                    if (it == element) {
+                        shows[element] = shows[element]!! + 1
+                    }
+                }
+            }
+            // final value is the shows * element
+            val value: Int = shows[element]!! * element
+            // add this to the sum
+            sum += value
+        }
+        return sum
     }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part1(testInput) == 11)
 
-    val testInputb = readInput("Day01_testb")
-    check(part2(testInputb) == 281)
-    // Read the input from the `src/Day01.txt` file.
+    val testInputb = readInput("Day01_test")
+    check(part2(testInputb) == 31)
+
     val input = readInput("Day01")
 
     part1(input).println()
